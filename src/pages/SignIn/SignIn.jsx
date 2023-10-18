27d64eb66f62f0f02322d6dafae5af406c7b7ecb
JAMSHEDWEBDEV <jamsheddc60@gmail.com>
2023-10-19 00:21:21 +0600

    added firebase signUp authentication

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,8 +1,47 @@
+import { Link } from "react-router-dom";
 
 const SignIn = () => {
+
+    const handleLogin = e =>{
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email,password);
+    }
+
     return (
-        <div>
-            <h1>This is sign In page</h1>
+        <div className="hero min-h-screen bg-base-200">
+            <div>
+                <div className="text-center mb-7">
+                    <h1 className="text-5xl font-bold">SignIn now!</h1>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleLogin} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">SignIn</button>
+                        </div>
+                        <div>
+                            <p>New User ? Please <Link to="/signUp" className="text-2xl text-blue-700 font-bold ml-2">SignUp</Link> </p>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,8 +1,69 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../Providers/AuthProvider";
+import swal from 'sweetalert';
 
 const SignUp = () => {
+    const {createUser} = useContext(AuthContext);
+
+    const handleRegister = e =>{
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name,email,password);
+
+        createUser(email,password)
+        .then(result =>{
+            if (result) {
+                swal("success!", "Your Registration successful!", "success");
+              }
+        })
+        .catch(error =>{
+            if (error) {
+                swal("Oops!", "This Email and Password already in used!", "error");
+              }
+        })
+    }
+
+
     return (
-        <div>
-            <h1>This is sign Up page</h1>
+        <div className="hero min-h-screen bg-base-200">
+            <div>
+                <div className="text-center mb-7">
+                    <h1 className="text-5xl font-bold">SignUp now!</h1>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleRegister} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name="name" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                            
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">SignUp</button>
+                        </div>
+                        <div>
+                            <p>Already have an account ? Please <Link to="/signIn" className="text-2xl text-blue-700 font-bold ml-2">SignIn</Link> </p>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
